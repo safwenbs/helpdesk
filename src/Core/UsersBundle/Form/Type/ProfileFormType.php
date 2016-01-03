c4a6c94242c0b8c76421c4cb0d84ad019eef3970
safwen <safoine.ben.salem@gmail.com>
2016-01-03 23:49:46 +0100

    FosUserBundle end manipulation

---
+++ src/Core/UsersBundle/Form/Type/ProfileFormType.php
@@ -0,0 +1,52 @@
+<?php
+
+/*
+ * This file is part of the FOSUserBundle package.
+ *
+ * (c) FriendsOfSymfony <http://friendsofsymfony.github.com/>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Core\UsersBundle\Form\Type;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+use Symfony\Component\Security\Core\Validator\Constraints\UserPassword;
+use FOS\UserBundle\Util\LegacyFormHelper;
+
+class ProfileFormType extends AbstractType
+{
+    
+   public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $this->buildUserForm($builder, $options);
+
+        $builder->add('current_password', 'password', array(
+            'label' => 'form.current_password',
+            'translation_domain' => 'FOSUserBundle',
+            'mapped' => false,
+            'constraints' => new UserPassword(),
+        ));
+    }
+    public function getName()
+    {
+        return 'helpdesk_user_profile';
+    }
+    public function getParent()
+    {
+        return 'fos_user_profile';
+    }
+
+   protected function buildUserForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+                ->add('firstname')
+                ->add('lastname')
+                ->add('username', null, array('label' => 'form.username', 'translation_domain' => 'FOSUserBundle'))
+                ->add('email', LegacyFormHelper::getType('Symfony\Component\Form\Extension\Core\Type\EmailType'), array('label' => 'form.email', 'translation_domain' => 'FOSUserBundle'))
+                ;
+    }
+}
